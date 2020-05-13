7f3823fc13b87b9a5cbac568e3a6c5e9eaae60ed
Austin FitzGerald <fitzgeralaus@uwplatt.edu>
2020-05-13 19:53:47 +0000

    [CSDS20T01-86] Our splash screen now displays errors messages related to failure to connect to the database or load the start screen. It could use SQLException subclasses to display what the error exactly is, but the user doesn't need that. Real application should implement logging.

--- source_files/src/main/java/ui/Launcher.java
+++ source_files/src/main/java/ui/Launcher.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -15,9 +16,11 @@ import java.sql.SQLException;
 
 /**
  * This class functions as a launcher. It will connect the client to the database and show them the start screen.
+ * Also handles failures in connecting to the database and loading the start screen.
  */
 public class Launcher extends Application {
     private static Stage stage;
+
     /**
      * This task is used to make a connection to the database before the program can be used by a user.
      */
@@ -41,38 +44,68 @@ public class Launcher extends Application {
         Launcher.stage.show();
     }
 
+    /**
+     * Entry point for the application. Called after the class is constructed and the init() method is called.
+     * This will delegate a task to connect to the database, it will handle success and failure of that task.
+     * @param stage New primary stage on application start
+     */
     @Override
     public void start(Stage stage) {
         Launcher.stage = stage;
 
         // show loading dialog until database is connected, then show start screen
-        Alert loadingDialog = createLoadingDialog();
-        task.setOnRunning((e) -> loadingDialog.show());
+        Alert dialog = createLoadingDialog();
+        task.setOnRunning((e) -> dialog.show());
         task.setOnSucceeded((e) -> {
-            loadingDialog.hide();
             try {
                 // show the start screen
                 Parent root = FXMLLoader.load(getClass().getResource("/StartScreen.fxml"));
                 stage.setTitle("Airport Management System");
                 stage.setScene(new Scene(root));
                 stage.show();
+                dialog.hide();
             } catch (Exception ex) {
-                ex.printStackTrace(); // TODO better exception handling
-                System.exit(0);
+                changeDialogToErrorDialog(dialog, "Could not load the Start Screen.");
             }
+        });
+        task.setOnFailed(e -> {
+            changeDialogToErrorDialog(dialog, "Could not connect to the database.");
+        });
+        task.setOnCancelled(e -> {
+            changeDialogToErrorDialog(dialog, "Connection cancelled.");
         });
         new Thread(task).start();
 
         Launcher.stage.setOnCloseRequest(e -> System.exit(0));
     }
 
+    /**
+     * Creates a dialog which is shown as sort of a splash screen. Lets the user know that the application is loading
+     * instead of showing them nothing.
+     * @return A new dialog, used to show that the system is loading
+     */
     private Alert createLoadingDialog() {
         Alert loadingDialog = new Alert(Alert.AlertType.INFORMATION);
         loadingDialog.setHeaderText(null);
         loadingDialog.setTitle("Airport Management System");
         loadingDialog.setContentText("Airport Management System is loading... Please Wait");
         loadingDialog.getDialogPane().lookupButton(ButtonType.OK).setVisible(false);
-        loadingDialog.initStyle(StageStyle.UNDECORATED);
+        loadingDialog.initStyle(StageStyle.UTILITY);
         return loadingDialog;
+    }
+
+    /**
+     * Changes the loading dialog to an error dialog. Displays a custom message of what the error was.
+     * @param dialog Dialog to change (the loading dialog)
+     * @param message What error message to display alongside the base message
+     */
+    private void changeDialogToErrorDialog(Alert dialog, String message)
+    {
+        dialog.setAlertType(Alert.AlertType.ERROR);
+        dialog.getButtonTypes().add(ButtonType.CLOSE);
+        dialog.getDialogPane().lookupButton(ButtonType.OK).setVisible(false);
+        Button closeButton = (Button) dialog.getDialogPane().lookupButton(ButtonType.CLOSE);
+        closeButton.setOnAction(eventCloseClicked -> System.exit(0));
+        dialog.setContentText("Airport Management System failed to load. " + message);
     }
 }
